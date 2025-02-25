b9e9f1738fa06c511c378870918575cf785f3132
Melvin Whitaker <awsome7mailman@gmail.com>
2025-02-24 18:36:43 -0700

    Finished M2C Presenter Demo!

---
+++ tweeter-web/test/presenter/AppNavbarPresenter.test.ts
@@ -0,0 +1,55 @@
+import { AuthToken } from "tweeter-shared";
+import { AppNavbarPresenter, AppNavbarView } from "../../src/presenters/AppNavbarPresenter";
+import { anything, capture, instance, mock, spy, verify, when } from "@typestrong/ts-mockito";
+import { UserService } from "../../src/model/service/UserService";
+
+describe("AppNavbarPresenter", () => {
+    let mockAppNavbarView: AppNavbarView;
+    let appNavbarPresenter: AppNavbarPresenter;
+    let mockUserService: UserService;
+    const authToken = new AuthToken("abc123", Date.now());
+
+    beforeEach(() => {
+        mockAppNavbarView = mock<AppNavbarView>();
+        const mockAppNavbarViewInstance = instance(mockAppNavbarView);
+
+        const appNavbarPresenterSpy = spy(new AppNavbarPresenter(mockAppNavbarViewInstance));
+        appNavbarPresenter = instance(appNavbarPresenterSpy);
+
+        mockUserService = mock<UserService>();
+        const mockUserServiceInstance = instance(mockUserService);
+
+        when(appNavbarPresenterSpy.userService).thenReturn(mockUserServiceInstance);
+    });
+
+    it("tells the view to display a logging out message", async () => {
+        await appNavbarPresenter.logOut(authToken);
+        verify(mockAppNavbarView.displayInfoMessage("Logging Out...", 0)).once();
+    });
+
+    it("calls logout on the user service with the correct auth token", async () => {
+        await appNavbarPresenter.logOut(authToken);
+        verify(mockUserService.logout(authToken)).once();
+    });
+
+    it("tells the view to clear the last info message and clear the user info when logout is successful", async () => {
+        await appNavbarPresenter.logOut(authToken);
+        
+        verify(mockAppNavbarView.displayErrorMessage(anything())).never();
+
+        verify(mockAppNavbarView.clearLastInfoMessage()).once();
+        verify(mockAppNavbarView.clearUserInfo()).once();
+    });
+
+    it("tells the view to display an error message and does not tell it to clear the last info message or clear the user info when logout fails", async () => {
+        const error = new Error("Could not log out the user");
+        when(mockUserService.logout(authToken)).thenThrow(error);
+        
+        await appNavbarPresenter.logOut(authToken);
+
+        verify(mockAppNavbarView.displayErrorMessage("Failed to log user out because of exception: Could not log out the user")).once();
+
+        verify(mockAppNavbarView.clearLastInfoMessage()).never();
+        verify(mockAppNavbarView.clearUserInfo()).never();
+    });
+});
