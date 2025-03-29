bc85e03a54d95f2b5336a5690e97ec088f83c156
Melvin Whitaker <awsome7mailman@gmail.com>
2025-03-28 21:03:29 -0600

    Login Lambda Working Fully With DynamoDB

--- tweeter-server/src/model/service/UserService.ts
+++ tweeter-server/src/model/service/UserService.ts
@@ -8,22 +8,35 @@ import {
 import { v4 as uuid } from "uuid";
 import bcrypt from "bcryptjs";
 import { DAOFactory } from "../dao/DAOFactory";
+import { UsersDAO } from "../dao/UsersDAO";
+import { ImageDAO } from "../dao/ImageDAO";
 
 export class UserService {
   private daoFactory: DAOFactory;
+  private usersDAO: UsersDAO;
+  private imageDAO: ImageDAO;
   constructor(daoFactory: DAOFactory) {
     this.daoFactory = daoFactory;
+    this.usersDAO = daoFactory.getUsersDAO();
+    this.imageDAO = daoFactory.getImageDAO();
   }
 
   public async login(
     alias: string,
     password: string
   ): Promise<[UserDto, AuthTokenDto]> {
-    // TODO: Replace with the result of calling the server
-    const user = FakeData.instance.firstUser;
+    //Get user in database
+    const userAndPassword = await this.usersDAO.getUser(alias);
 
-    if (user === null) {
-      throw new Error("Invalid alias or password");
+    if (userAndPassword === undefined) {
+      throw new Error("[Bad Request] Invalid alias");
+    }
+
+    const [user, hashedPassword] = userAndPassword;
+
+    //Check password w/ hashedPassword
+    if (!(await bcrypt.compare(password, hashedPassword))) {
+      throw new Error("[Bad Request] Invalid password");
     }
 
     return [user.dto, FakeData.instance.authToken.dto];
@@ -37,20 +50,28 @@ export class UserService {
     imageStringBase64: string,
     imageFileExtension: string
   ): Promise<[UserDto, AuthTokenDto]> {
+    //Check for invalid parameters
+    if (!firstName || !lastName || !alias || !password) {
+      throw new Error("[Bad Request] Missing required information");
+    }
+    //Check if user is already in database
+    const registeredUser = await this.usersDAO.getUser(alias);
+    if (registeredUser !== undefined) {
+      throw new Error("[Bad Request] Alias already taken");
+    }
+
     //Uploading image to database
     const fileName = uuid().toString() + imageFileExtension;
-    const imageUrl = await this.daoFactory
-      .getImageDAO()
-      .putImage(fileName, imageStringBase64);
+    const imageUrl = await this.imageDAO.putImage(fileName, imageStringBase64);
     //Hashing password
     const salt = await bcrypt.genSalt();
     const hashedPassword = await bcrypt.hash(password, salt);
     //Storing user in database
     const user = new User(firstName, lastName, alias, imageUrl);
-    await this.daoFactory.getUsersDAO().putUser(user, hashedPassword);
+    await this.usersDAO.putUser(user, hashedPassword);
 
     if (user === null) {
-      throw new Error("Invalid registration");
+      throw new Error("[Bad Request] Invalid registration");
     }
 
     return [user.dto, FakeData.instance.authToken.dto];
